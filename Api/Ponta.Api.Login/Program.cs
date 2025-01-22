6581ebf501bd342cc29f49e22b89c80f94b2a9e0
Rodrigo Martins Moraes <rodrigo_moraes@hotmail.com.br>
2025-01-22 15:46:19 -0300

    login api

--- Api/Ponta.Api.Login/Program.cs
+++ Api/Ponta.Api.Login/Program.cs
@@ -1,8 +1,17 @@
+using Ponta.Contexto.Usuario.Contexto;
+using Ponta.Contexto.Usuario.Interfaces;
+using Ponta.Contexto.Usuario.Repositorio;
+using Ponta.Servico.Login;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddScoped<IServicoLogin, ServicoLogin>();
+builder.Services.AddScoped<IRepositorioUsuario, RepositorioUsuario>();
+builder.Services.AddScoped<IContextoUsuario, ContextoUsuario>();
 
 var app = builder.Build();
 
@@ -14,28 +23,13 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
+app.MapPost("/{login}/{senha}", async (IServicoLogin servico, string login, string senha) =>
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    return await servico.LoginAsync(login, senha);
 })
-.WithName("GetWeatherForecast");
+.WithName("Login");
 
-app.Run();
+await app.RunAsync();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+
